73bcb889437871d6099b0d741dde03996080e0aa
vquennessen <viquennessen@gmail.com>
2023-07-28 11:45:41 -0400

    updated cluster runs

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -1,13 +1,8 @@
 ### cluster runs
 
-# TO DO:
-# - average temperature during incubation period
-# - stable age distribution of population for each mating function at current 
-#          average temperature
-# - initialize population with that for 50 - 80 males and 180 - 300 females
-
 # load libraries
 library(parallel)
+library(dplyr)
 
 # set working directory
 # setwd('~/Projects/iliketurtles3/code')
@@ -15,20 +10,20 @@ library(parallel)
 source('run_base_model.R')
 source('base_model.R')
 source('initialize_arrays.R')
-source('initialize_population2.R')
+source('initialize_population.R')
 source('reproduction.R')
 source('pop_dynamics.R')
 
 # number of simulations to run
-num_sims <- 10000
+num_sims <- 300
 
 # number of cores to run
-mc <- 16
+mc <- 24
 
 # scenarios <- c(3.5)  # total temp increases
-scenarios <- c(1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12)  # total temp increases
+scenarios <- c(1.5)  # total temp increases
 
-betas <- c(1, 1.35, 1.94, 3.1, 6.57, 8.31, 11.19, 16.94, 34.14)
+betas <- c(8.31, 11.19, 16.94, 34.14)
 # betas <- c(34.14)
 
 ########### do the runs ########################################################
