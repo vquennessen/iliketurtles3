d84e1bfc0997709676916152b2a6e36a9628d541
vquennessen <viquennessen@gmail.com>
2023-11-01 22:57:13 -0700

    fixed machine_runs (forgot in last commit)

--- code/machine_runs.R
+++ code/machine_runs.R
@@ -16,17 +16,37 @@ source('initialize_population.R')
 source('reproduction.R')
 source('pop_dynamics.R')
 
-# number of simulations to run
-num_sims <- c(1, 2, 3, 4)
-
 # scenarios <- c(10.5, 12)  # total temp increases
 scenarios <- c(1.5)  # total temp increases
 
 betas <- c(16.94, 34.14)
 # betas <- c(1, 1.35)
 
+# number of simulations to run
+num_sims <- c(12, 22, 32, 42, 52, 62, 72, 82, 92, 102)
+
+# make dataframe of all combinations of arguments
+DF <- expand.grid(scenarios, betas, num_sims)
+
+# initialize empty arguments list
+arguments <- list()
+
+# fill in arguments list with dataframe rows
+for (i in 1:nrow(DF)) {
+  
+  arguments[i] <- list(as.numeric(DF[i, ]))
+  
+}
+
 ########### do the runs ########################################################
-lapply(X = scenarios, 
-       FUN = run_base_model,
-       num_sims, 
-       betas)  
+# lapply(X = scenarios, 
+#        FUN = run_base_model,
+#        num_sims, 
+#        betas)  
+# 
+# mapply(FUN = run_base_model, 
+#        scenarios,
+#        num_sims, 
+#        betas) 
+
+mclapply(X = arguments, FUN = run_base_model)
