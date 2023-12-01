c14507cefdf47fb4418c40579a66e7fea1c6cae7
vquennessen <viquennessen@gmail.com>
2023-12-01 14:14:12 -0800

    fixed variance analysis

--- code/variance_analysis.R
+++ code/variance_analysis.R
@@ -17,71 +17,71 @@ setwd("C:/Users/vique/Documents/Projects/iliketurtles3/code/")
 
 # set number of sims
 num_sims <- 10000
-num_medians <- 100
-num_variances <- 100
+num_variances <- 1000
+scenarios <- c(1.5, 3)
+betas <- c(11.19, 6.57)
 
 # load datafiles - total abundance for 3C scenario with beta = 6.57 and 
 # 1.5C scenario with beta = 11.19
 base <- 'C:/Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/model output/'
-load(paste(base, '2023_07_21_newdata_3gen/3C/beta6.57/10000_abundance_total.Rda', sep = ''))
-v3_6_57 <- sims_abundance_total
-load(paste(base, '2023_07_21_newdata_3gen/1.5C/beta11.19/10000_abundance_total.Rda', sep = ''))
-v1_5_11_19 <- sims_abundance_total
+# v3_6_57 <- sims_abundance_total
+# load(paste(base, '2023_07_21_newdata_3gen/1.5C/beta11.19/10000_abundance_total.Rda', sep = ''))
+# v1_5_11_19 <- sims_abundance_total
 
 # set different sample sizes
 sample_size <- seq(from = 1000, to = num_sims, by = 100)
 
 # initialize variance dataframe 
-variance_df <- data.frame(Sample.Size = sample_size,
-                          A.Variance1 = rep(NA, length(sample_size)), 
-                          A.Variance2 = rep(NA, length(sample_size)))
+variance_df <- data.frame(Scenario = rep(c('1.5C', '3C'), each = length(sample_size)),
+                          Beta = rep(c(11.19, 6.57), each = length(sample_size)),
+                          Sample.Size = rep(sample_size, times = length(scenarios)),
+                          Variance = rep(NA, length(sample_size)))
 
-A_variance1 <- rep(0, num_variances)
-A_variance2 <- rep(0, num_variances)
 
-A_medians1 <- rep(0, num_medians)
-A_medians2 <- rep(0, num_medians)
-
-for (i in 1:length(sample_size)) {
+for (s in 1:length(scenarios)) {
   
-  for (j in 1:num_variances) {
+  # load total abundance for scenario
+  load(paste(base, '2023_07_21_newdata_3gen/', scenarios[s], 'C/beta', betas[s], 
+             '/10000_abundance_total.Rda', sep = ''))
+
+  for (i in 1:length(sample_size)) {
     
-    for (k in 1:num_medians) {
+    # reset variances vector
+    variances <- rep(0, num_variances)
+
+    for (j in 1:num_variances) {
       
       indices <- sample(1:num_sims, sample_size[i])
-      sampled_Atotal_1 <- v3_6_57[256, indices]
-      sampled_Atotal_2 <- v1_5_11_19[256, indices]
-      
-      # calculate medians 
-      A_medians1[k] <- median(sampled_Atotal_1)
-      A_medians2[k] <- median(sampled_Atotal_2)
-      
+      variances[j] <- var(sims_abundance_total[256, indices])
+
     }
     
-    A_variance1[j] <- var(A_medians1)
-    A_variance2[j] <- var(A_medians2)
-    
+    # calculate median
+    index <- (s - 1)*length(sample_size) + i
+    variance_df$Variance[index] <- median(variances)
+
   }
-  
-  variance_df$A.Variance1[i] <- median(A_variance1)
-  variance_df$A.Variance2[i] <- median(A_variance2)
   
 }
 
-A <- ggplot(data = variance_df, aes(x = Sample.Size, y = A.Variance1)) +
+A <- ggplot(data = subset(variance_df, Scenario == '1.5C'), aes(x = Sample.Size, y = Variance)) +
   geom_point() +
-  ggtitle('Total Abundance Variance 1')
+  ggtitle('Total Abundance Variance 1.5C, beta = 11.19')
 
-M <- ggplot(data = variance_df, aes(x = Sample.Size, y = A.Variance2)) +
-  geom_line() +
-  ggtitle('Total Abundance Variance 1')
+B <- ggplot(data = subset(variance_df, Scenario == '3C'), aes(x = Sample.Size, y = Variance)) +
+  geom_point() + 
+  ggtitle('Total Abundance Variance 3C, beta = 6.57')
 
-ggsave(filename = paste('Abundance_total_variance1.png', sep = ''), 
+ggsave(filename = paste('Abundance_total_variance_1.5_11.19.png', sep = ''), 
        plot = A, 
        path = 'C:/Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/model output/variance runs')
 
-ggsave(filename = paste('Abundance_total_variance2.png', sep = ''), 
-       plot = M, 
+ggsave(filename = paste('Abundance_total_variance_3_6.57.png', sep = ''),
+       plot = B,
        path = 'C:/Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/model output/variance runs')
 
 toc()
+# num_variances = 100
+# 16.63 seconds
+# num_variances = 1000
+# 140.1 seconds
