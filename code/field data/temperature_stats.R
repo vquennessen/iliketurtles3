e0d7c0cd57b8e3264d96ef801b56c1717bc0126b
vquennessen <viquennessen@gmail.com>
2024-01-29 15:53:12 -0800

    updated hatching success data exploration for howard et al. 2015 vs. laloe et al. 2017 and running base model

--- code/field data/temperature_stats.R
+++ code/field data/temperature_stats.R
@@ -15,7 +15,8 @@ library(ggplot2)
 # load nest object
 load("~/Projects/iliketurtles3/data/nests.Rdata")
 nests2 <- nests %>%
-  select(Season, N_NINHO, TEMP_LOGGER_DEPLOYED, TEMP_LOGGER_REMOVED)
+  select(Season, N_NINHO, TEMP_LOGGER_DEPLOYED, TEMP_LOGGER_REMOVED, 
+         Hatching_success)
 
 # define mode function
 # Create mode() function to calculate mode
@@ -31,14 +32,16 @@ stat_mode <- function(x, na.rm = TRUE) {
 
 # load in and clean up temp data
 
-# initialize dataframe for each nest, with season, type of loger, CTE, 
-# and month of incubation
+# initialize dataframe for each nest, with season, type of logger, CTE, 
+# month of incubation, and hatching success
 incubation <- data.frame(nest_number = NA, 
                          season = NA, 
                          logger = NA, 
                          logger_number = NA, 
                          month = NA, 
-                         CTE = NA)
+                         CTE = NA, 
+                         CTE_all = NA, 
+                         hatch_success = NA)
 
 ### season 1
 seasons <- c('season 1', 'season 2', 'season 3', 'season 4')
@@ -56,11 +59,13 @@ for (s in 1:length(seasons)) {
   
   
   tempDF <- data.frame(nest_number = rep(NA, length(filenames)), 
-                           season = rep(NA, length(filenames)), 
-                           logger = rep(NA, length(filenames)), 
-                           logger_number = rep(NA, length(filenames)), 
-                           month = rep(NA, length(filenames)), 
-                           CTE = rep(NA, length(filenames)))
+                       season = rep(NA, length(filenames)), 
+                       logger = rep(NA, length(filenames)), 
+                       logger_number = rep(NA, length(filenames)), 
+                       month = rep(NA, length(filenames)), 
+                       CTE = rep(NA, length(filenames)), 
+                       CTE_all = rep(NA, length(filenames)),
+                       hatch_success = rep(NA, length(filenames)))
   
   # for each file
   for (f in 1:length(filenames)) {
@@ -76,7 +81,7 @@ for (s in 1:length(seasons)) {
     tempDF$season[f] <- parse_number(seasons[s])
     tempDF$logger[f] <- temp$logger[1]
     tempDF$logger_number[f] <- temp$number[1]
-    
+
     # extract correct row from nests2
     nest <- subset(nests2, Season == years[s] & N_NINHO == parse_number(as.character(temp$nest[1])))
     
@@ -97,7 +102,16 @@ for (s in 1:length(seasons)) {
     tempDF$month[f] <- stat_mode(as.numeric(month(incubation_dates$datetime)))
     
     # add CTE just as mean temperature for now
-    tempDF$CTE[f] <- mean(parse_number(as.character((incubation_dates$temperature), na.rm = TRUE)))
+    tempDF$CTE[f] <- mean(parse_number(as.character((incubation_dates$temperature), 
+                                                    na.rm = TRUE)))
+    
+    # add CTE just as mean temperature for now
+    tempDF$CTE_all[f] <- mean(parse_number(as.character((temp$temperature), 
+                                                        na.rm = TRUE)))
+    
+    # add hatching success
+    tempDF$hatch_success[f] <- nest$Hatching_success
+      
     
     #### TO DO calculate CTE
     # M <- mean(incubation_dates$temperature)
@@ -117,9 +131,11 @@ save(incubation, file = '../../data/incubation.Rdata')
 
 data <- incubation %>%
   filter(season != 1)  %>%
-  summarise(sd(CTE))
+  na.omit(hatch_success) 
 
-# sd = 0.841012
+sd_CTE <- data %>%
+  summarise(sd(CTE, na.rm = TRUE))
+# sd = 0.8333391
   
 ggplot(data, aes(x = as.factor(month), y = CTE, col = as.factor(season))) +
   geom_boxplot()
@@ -133,4 +149,81 @@ stats <- data %>%
 
 # calculate average temperature given proportion and CTE in each month
 sum(stats$mean*stats$frequency)
-# 31.80387
+# 31.80488
+# with median = 31.8335
+
+# calculate relationship between CTE and hatching success
+data2 <- incubation %>%
+  select(CTE, CTE_all, hatch_success) %>%
+  na.omit()
+
+mod1 <- lm(data = data2, hatch_success ~ CTE)
+summary(mod1)
+
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) -1.35619    0.55186  -2.457 0.014830 *  
+#   CTE          0.06754    0.01730   3.903 0.000129 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.1994 on 203 degrees of freedom
+# Multiple R-squared:  0.06981,	Adjusted R-squared:  0.06522 
+# F-statistic: 15.23 on 1 and 203 DF,  p-value: 0.0001292
+
+# howard et al. 2015 - 4th order polynomial
+mod2 <- lm(data = data2, hatch_success ~ poly(CTE, 4))
+summary(mod2)
+
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)    0.79709    0.01274  62.586  < 2e-16 ***
+#   poly(CTE, 4)1  0.77817    0.18235   4.267 3.05e-05 ***
+#   poly(CTE, 4)2 -1.11916    0.18235  -6.137 4.43e-09 ***
+#   poly(CTE, 4)3  0.25160    0.18235   1.380   0.1692    
+# poly(CTE, 4)4 -0.32112    0.18235  -1.761   0.0798 .  
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.1823 on 200 degrees of freedom
+# Multiple R-squared:  0.2334,	Adjusted R-squared:  0.218 
+# F-statistic: 15.22 on 4 and 200 DF,  p-value: 6.988e-11
+
+x1 <- seq(from = min(data2$CTE), to = max(data2$CTE), by = 0.01)
+y1 <- (0.79709 + 0.77817*x - 1.11916*x^2 + 0.25160*x^3 - 0.32112*x^4) / 100 
+
+prd <- data.frame(CTE = seq(from = range(data2$CTE)[1], to = range(data2$CTE)[2], 
+                            length.out = 100))
+err <- predict(mod2, newdata = prd, se.fit = TRUE)
+
+prd$lci <- err$fit - 1.96 * err$se.fit
+prd$fit <- err$fit
+prd$uci <- err$fit + 1.96 * err$se.fit
+
+
+# laloe et al. 2017
+A <- 0.86
+beta <- 1.7
+t0 <- 32.7
+x2 <- seq(from = min(data2$CTE_all), to = max(data2$CTE_all), by = 0.01)
+y2 <- A / (1 + exp(beta * (x2 - t0)))
+laloe <- data.frame(CTE = x2, hatch_success = y2)
+
+ggplot(prd, aes(x = CTE, y = fit)) +
+  theme_bw() +
+  geom_line() +
+  geom_smooth(aes(ymin = lci, ymax = uci), stat = "identity") +
+  geom_point(data = data2, aes(x = CTE, y = hatch_success)) +
+  ylab('Hatching Success') +
+  xlab('Middle third average incubation temperature (C)') +
+  ggtitle("Howard et al. 2015")
+
+ggplot(data2, aes(x = CTE_all, y = hatch_success)) +
+  theme_bw() +
+  geom_point() +
+  geom_line(data = laloe, aes(x = CTE, y = hatch_success), 
+            lwd = 2) +
+  ylab('Hatching Success') +
+  xlab('Average incubation temperature (C)')
+  ggtitle("Laloe et al. 2017")
+
--- code/run_base_model.R
+++ code/run_base_model.R
@@ -12,34 +12,37 @@ run_base_model <- function(arguments) {
   F_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.799)  # survival per stage - F
   M_survival_years <- c(1, 2, 7, 12, 1)                 # years per stage - M
   M_survival_values <- c(0.35, 0.8, 0.85, 0.85, 0.799)  # survival per stage - M
-  demographic_stochasticity <- TRUE                 # demographic stochasticity
-  age_maturity <- 23                                # age at first reproduction
-  remigration_int <- 3.8701                          # remigration interval
-  nests_mu <- 4.945312                              # mean # of nests/F/season
-  nests_sd <- 2.089752                              # sd of # of nests/F/season
-  eggs_mu <- 100.6486                               # mean number of eggs/nest
-  eggs_sd <- 23.08206                               # sd of number of eggs/nest
-  hatch_success_mu <- 0.8241024                     # mean of hatching success
-  hatch_success_a <- 2.089414                       # beta CDF shape 1 par -> HS
-  hatch_success_b <- 0.4496393                      # beta CDF shape 2 par -> HS
-  hatch_success_stochasticity <- TRUE               # stochasticity in HS?
-  T_piv <- 29.2                                     # pivotal temperature mean
+  demographic_stochasticity <- TRUE         # demographic stochasticity
+  age_maturity <- 23                        # age at first reproduction
+  remigration_int <- 3.8701                 # remigration interval
+  nests_mu <- 4.945312                      # mean # of nests/F/season
+  nests_sd <- 2.089752                      # sd of # of nests/F/season
+  eggs_mu <- 100.6486                       # mean number of eggs/nest
+  eggs_sd <- 23.08206                       # sd of number of eggs/nest
+  hatch_success_mode <- 'polynomial'        # other options = logistic, static
+  hatch_success_mu <- 0.8241024             # mean of hatching success
+  hatch_success_a <- 2.089414               # stochastic beta CDF shape 1 par
+  hatch_success_b <- 0.4496393              # stochastic beta CDF shape 2 par
+  hatch_success_A <- 0.86                   # logistic by temp - A
+  hatch_success_beta <- -1.7                # logistic by temp - beta
+  hatch_success_t0 <- 32.7                  # logistic by temp - t0
+  T_piv <- 29.2                             # pivotal temperature mean
   T_piv_sd <- 
-  TRT <- c(27.6, 31.4)                              # Transitional range temps
-  logit_a <- 41.362228                              # temp -> prop of males a
-  logit_b <- -1.415462                              # temp -> prop of males b
-  F_initial <- 170                                  # initial adult F
-  M_initial <- 30                                   # initial adult M
+  TRT <- c(27.6, 31.4)                      # Transitional range temps
+  logit_a <- 41.362228                      # temp -> prop of males a
+  logit_b <- -1.415462                      # temp -> prop of males b
+  F_initial <- 170                          # initial adult F
+  M_initial <- 30                           # initial adult M
   
   # climate data
-  temp_mu <- 31.80387                               # base incubation temp mean
-  temp_sd <- 0.841012                               # base incubation temp sd
-  climate_stochasticity <- FALSE                    # whether or not to add in
+  temp_mu <- 31.80387                       # base incubation temp mean
+  temp_sd <- 0.841012                       # base incubation temp sd
+  climate_stochasticity <- FALSE            # whether or not to add in
   
   # model parameters
-  start_year <- 2023                                # first year to simulate
-  end_year <- start_year + 3*max_age                # last year to simulate
-  evolution <- TRUE                                 # whether evolution is turned on
+  start_year <- 2023                        # first year to simulate
+  end_year <- start_year + 3*max_age        # last year to simulate
+  evolution <- TRUE                         # whether evolution is turned on
   
   # dimensions
   A <- max_age
