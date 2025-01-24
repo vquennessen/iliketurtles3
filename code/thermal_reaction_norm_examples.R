bb0acb0bd01d713acc9e1a369b9b47e68d8cf0d1
vquennessen <viquennessen@gmail.com>
2025-01-23 16:32:08 -0800

    improving thermal reaction norm figure

--- code/thermal_reaction_norm_examples.R
+++ code/thermal_reaction_norm_examples.R
@@ -10,7 +10,7 @@ k1 <- -1.4
 y1 <- 1/(1 + exp(-k1*(x - t_piv1)))
 
 # embryogrowth (all greens)
-t_piv2 <- 29.4
+t_piv2 <- 29.2
 k2 <- -0.56
 y2 <- 1/(1 + exp(-k2*(x - t_piv2)))
 
@@ -20,19 +20,23 @@ TRN <- data.frame(Temperature = x,
                   Proportion_Male = c(y1, y2))
 
 # plot
-ggplot(data = TRN, aes(x = Temperature, y = Proportion_Male, col = Model)) +
+ggplot(data = TRN, aes(x = Temperature, y = Proportion_Male, 
+                       col = Model, lty = Model)) +
   geom_line(lwd = 2) +
-  geom_hline(yintercept = c(0.05, 0.50, 0.95), lty = 2, lwd = 2) +
-  ylab('Proportion male') +
-  xlab('Temperature (C)') +
-  ggtitle('Thermal Reaction Norm') +
+  geom_hline(yintercept = c(0.05, 0.50, 0.95), lty = 1, lwd = 1.5) +
+  geom_vline(xintercept = t_piv1, col = 'darkgrey', lty = 1, lwd = 1.5) +
+  geom_line(lwd = 2) +
+  ylab('Proportion hatchlings male') +
+  xlab('Temperature (\u00B0C)') +
+  # ggtitle('Thermal Reaction Norm') +
   theme_gray() +
   theme(axis.title.y = element_text(margin = margin(r = 15, l = 10)), 
         axis.title.x = element_text(margin = margin(t = 15, b = 10)), 
-        plot.title = element_text(margin = margin(b = 10, t = 10), 
-                                  size = 30), 
+        # plot.title = element_text(margin = margin(b = 10, t = 10), 
+        #                           size = 30), 
         axis.text = element_text(size = 20), 
         axis.title = element_text(size = 25), 
         legend.title = element_text(size = 20), 
         legend.text = element_text(size = 20), 
-        legend.position = c(0.8, 0.7))
+        legend.position = c(0.8, 0.7), 
+        legend.key.width = unit(3.75, "line"))
