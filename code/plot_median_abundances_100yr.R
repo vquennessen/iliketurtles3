2c6e5c2b0e130ebb12d891efbd32dedca28864f1
vquennessen <viquennessen@gmail.com>
2025-02-26 14:37:13 -0800

    still working on that first figure

--- code/plot_median_abundances_100yr.R
+++ code/plot_median_abundances_100yr.R
@@ -6,6 +6,7 @@ rm(list = ls())
 # load libraries
 library(ggplot2)
 library(matrixStats)
+library(readr)
 
 # source functions
 source('~/Projects/iliketurtles3/code/mating function/OSRs_to_betas.R')
@@ -37,6 +38,7 @@ SDF <- data.frame(Scenario = NULL,
                   OSR = NULL,
                   Combo = NULL,
                   Year = NULL,
+                  Temperature = NULL,
                   Age = NULL,
                   Value = NULL,
                   Median = NULL, 
@@ -47,17 +49,21 @@ SDF <- data.frame(Scenario = NULL,
 # for each scenario
 for (s in 1:length(scenarios)) {
   
+  temp_increases <- parse_number(scenarios)
+  
+  temps <- seq(from = 31.8, to = 31.8 + temp_increases[s], length = 100)
+  
   # for each osr
   for (b in 1:length(betas)) {
     
     # load in sims N
-    load(paste('C:/Users/Vic/Box Sync/Quennessen_Thesis/PhD Thesis/', 
+    load(paste('C:/Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/', 
                'model output/temp_stochasticity/P_base/', 
                scenarios[s], '/', beta_names[b], '/10000_N.Rda', 
                sep = ''))
     
     # load in sims OSR
-    load(paste('C:/Users/Vic/Box Sync/Quennessen_Thesis/PhD Thesis/', 
+    load(paste('C:/Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/', 
                'model output/temp_stochasticity/P_base/', 
                scenarios[s], '/', beta_names[b], '/10000_OSR.Rda', 
                sep = ''))
@@ -71,8 +77,9 @@ for (s in 1:length(scenarios)) {
     # hathclings total
     hatchlings_total <- hatchlings_F + hatchlings_M
     
-    # extract hatchling sex ratios
+    # extract hatchling sex ratios, remove NaNs
     hatchling_sex_ratio <- hatchlings_M / (hatchlings_F + hatchlings_M)
+    hatchling_sex_ratio[!is.finite(hatchling_sex_ratio)] <- NA
     
     # extract mature F
     mature_F <- colSums(sims_N[1, , , ]*M, dims = 1)
@@ -83,9 +90,12 @@ for (s in 1:length(scenarios)) {
     # mature total
     mature_total <- mature_F + mature_M
     
-    # mature sex ratio
+    # mature sex ratio, remove infinite values
     OSR <- sims_OSR
+    OSR[!is.finite(OSR)] <- NA
     
+    # remove NaNs
+
     # objects
     things <- list(mature_total, hatchlings_total, 
                        OSR, hatchling_sex_ratio)
@@ -97,9 +107,10 @@ for (s in 1:length(scenarios)) {
                            Combo = paste(scenarios[s], ' - OSR ', osrs[b], 
                                          sep = ''),
                            Year = 1:100,
+                           Temperature = temps,
                            Age = ages[t],
                            Value = values[t],
-                           Median = rowMedians(things[[t]]), 
+                           Median = rowMedians(things[[t]], na.rm = TRUE), 
                            Q25 = rowQuantiles(things[[t]], 
                                               prob = 0.25, 
                                               na.rm = TRUE), 
@@ -112,8 +123,8 @@ for (s in 1:length(scenarios)) {
       SDF <- rbind(SDF, subset)
       
       # update tracker
-      print(paste(scenarios[s], beta_names[b], abundance_names[a], 'all done!', 
-                  sep = ' - '))
+      print(paste(scenarios[s], beta_names[b], ages[t], values[t], 
+                  'all done!', sep = ' - '))
       print(paste('length SDF = ', nrow(SDF), sep = ''))
       
     }
@@ -135,18 +146,19 @@ median_abundances_to_plot_over_time$Scenario <-
   factor(median_abundances_to_plot_over_time$Scenario)
 
 mature <- subset(median_abundances_to_plot_over_time, 
-                 Abundance %in% c('Mature F', 'Mature M'))
+                 Age == 'Mature')
 
 hatchling <- subset(median_abundances_to_plot_over_time, 
-                 Abundance %in% c('Hatchling F', 'Hatchling M'))
+                 Age == 'Hatchling')
+
+sex_ratios <- subset(median_abundances_to_plot_over_time, 
+                     Value == 'Sex Ratio')
 
 # plot figure - mature abundances
-figA <- ggplot(data = median_abundances_to_plot_over_time, 
+figA <- ggplot(data = mature, 
                 aes(x = Year, 
                     y = Median, 
                     color = Combo)) + 
-  facet_wrap(vars(Abundance), 
-             scales = 'free') +
   geom_ribbon(aes(ymin = Q25,
                   ymax = Q75, 
                   col = NULL, 
@@ -157,7 +169,7 @@ figA <- ggplot(data = median_abundances_to_plot_over_time,
   # scale_color_manual(values = c('#F8766D', '#00BFC4')) + 
   xlab('Year') +
   ylab('Abundance') +
-  ggtitle('temperature stochasticity; median mature abundances over time + IQR') +
+  ggtitle('median mature abundances over time + IQR') +
   theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
   theme(axis.title.x = element_text(size = 13, vjust = -3)) +
   theme(axis.title.y = element_text(size = 13, vjust = 4)) +
@@ -165,11 +177,81 @@ figA <- ggplot(data = median_abundances_to_plot_over_time,
   theme(strip.text = element_text(size = 12)) +
   theme(title = element_text(size = 13))
 
-# save to file
-ggsave(plot = figA, 
-       filename = paste('median_mature_abundances.png', sep = ''),
-       path = '~/Projects/iliketurtles3/figures/',
-       width = 9, height = 8)
+# plot figure - hatchling abundances
+figB <- ggplot(data = hatchling, 
+               aes(x = Year, 
+                   y = Median, 
+                   color = Combo)) + 
+  geom_ribbon(aes(ymin = Q25,
+                  ymax = Q75, 
+                  col = NULL, 
+                  fill = Combo),
+              alpha = 0.25,
+              show.legend = FALSE) +
+  geom_path(lwd = 1) +
+  # scale_color_manual(values = c('#F8766D', '#00BFC4')) + 
+  xlab('Year') +
+  ylab('Abundance') +
+  ggtitle('median hatchling abundances over time + IQR') +
+  theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
+  theme(axis.title.x = element_text(size = 13, vjust = -3)) +
+  theme(axis.title.y = element_text(size = 13, vjust = 4)) +
+  theme(axis.text = element_text(size = 10)) +
+  theme(strip.text = element_text(size = 12)) +
+  theme(title = element_text(size = 13))
+
+# plot figure - sex ratios
+figC <- ggplot(data = sex_ratios, 
+               aes(x = Year, 
+                   y = Median, 
+                   color = Combo, 
+                   lty = Age)) + 
+  geom_ribbon(aes(ymin = Q25,
+                  ymax = Q75, 
+                  col = NULL, 
+                  fill = Combo),
+              alpha = 0.25,
+              show.legend = FALSE) +
+  geom_path(lwd = 1) +
+  # scale_color_manual(values = c('#F8766D', '#00BFC4')) + 
+  xlab('Year') +
+  ylab('Abundance') +
+  ggtitle('median sex ratios over time + IQR') +
+  theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
+  theme(axis.title.x = element_text(size = 13, vjust = -3)) +
+  theme(axis.title.y = element_text(size = 13, vjust = 4)) +
+  theme(axis.text = element_text(size = 10)) +
+  theme(strip.text = element_text(size = 12)) +
+  theme(title = element_text(size = 13))
+
+# plot figure - temperatures
+figD <- ggplot(data = median_abundances_to_plot_over_time, 
+               aes(x = Year, 
+                   y = Temperature, 
+                   color = Combo)) + 
+  geom_ribbon(aes(ymin = Q25,
+                  ymax = Q75, 
+                  col = NULL, 
+                  fill = Combo),
+              alpha = 0.25,
+              show.legend = FALSE) +
+  geom_path(lwd = 1) +
+  # scale_color_manual(values = c('#F8766D', '#00BFC4')) + 
+  xlab('Year') +
+  ylab('Abundance') +
+  ggtitle('temperatures over time + IQR') +
+  theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
+  theme(axis.title.x = element_text(size = 13, vjust = -3)) +
+  theme(axis.title.y = element_text(size = 13, vjust = 4)) +
+  theme(axis.text = element_text(size = 10)) +
+  theme(strip.text = element_text(size = 12)) +
+  theme(title = element_text(size = 13))
+
+# # save to file
+# ggsave(plot = figA, 
+#        filename = paste('median_mature_abundances.png', sep = ''),
+#        path = '~/Projects/iliketurtles3/figures/',
+#        width = 9, height = 8)
 
 # # plot figure - hatchling abundances
 # figB <- ggplot(data = hatchling, 
