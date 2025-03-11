3abc3b646e9f126bb4f44e444bc9e780fc1664b7
vquennessen <viquennessen@gmail.com>
2025-03-11 11:43:02 -0700

    better thermal reaction norm examples figure

--- code/thermal_reaction_norm_examples.R
+++ code/thermal_reaction_norm_examples.R
@@ -30,24 +30,32 @@ fig <- ggplot(data = TRN, aes(x = Temperature, y = Proportion_Male,
                               col = Population, lty = Population)) +
   geom_hline(yintercept = c(0.05, 0.50, 0.95), lwd = 1, lty = 1) +
   geom_vline(xintercept = t_piv1, col = 'darkgrey', lwd = 1, lty = 1) +
+  geom_vline(xintercept = 27, col = '#00BFC4', lty = 5, 
+             lwd = 0.75, alpha = 0.5) +
+  geom_vline(xintercept = 31.3, col = '#00BFC4', lty = 5, 
+             lwd = 0.75, alpha = 0.5) +
+  geom_vline(xintercept = 34.4, col = '#F8766D', lty = 4, 
+             lwd = 0.75, alpha = 0.5) +
+  geom_vline(xintercept = 23.8, col = '#F8766D', lty = 4, 
+             lwd = 0.75, alpha = 0.5) +
   geom_line(lwd = 2) +
   scale_color_manual(values = c('#F8766D', '#00BFC4')) +
   scale_linetype_manual(values = c(4, 5)) +
   ylab('Proportion hatchlings male') +
   xlab('Temperature (\u00B0C)') +
   theme_bw() +
-  theme(axis.text = element_text(size = 15), 
-        axis.title = element_text(size = 20),
-        # legend.position = 'none', +
-        legend.text = element_text(size = 15),
-        legend.title = element_text(size = 20),
-        legend.position = 'inside', 
-        legend.position.inside = c(0.75, 0.7),
-        legend.key.width = unit(8, "line")) +
-  annotate("text", x = 39.4, y = 0.08, label = "0.05", size = 6) +
-  annotate("text", x = 39.4, y = 0.53, label = "0.50", size = 6) +
-  annotate("text", x = 39.4, y = 0.98, label = "0.95", size = 6) +
-  annotate("text", x = 28.6, y = 0, label = "29.2", size = 6)
+  theme(axis.text = element_text(size = 12), 
+        axis.title = element_text(size = 15),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 15),
+        legend.position = 'top',
+        # legend.position = 'inside', 
+        # legend.position.inside = c(0.8, 0.75),
+        legend.key.width = unit(9.63, "line")) +
+  annotate("text", x = 39.4, y = 0.1, label = "0.05", size = 5) +
+  annotate("text", x = 39.4, y = 0.55, label = "0.50", size = 5) +
+  annotate("text", x = 39.4, y = 1.00, label = "0.95", size = 5) +
+  annotate("text", x = 28.1, y = 0, label = "29.2\u00B0C", size = 5)
 
 
 fig
