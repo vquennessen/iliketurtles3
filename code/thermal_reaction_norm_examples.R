325f20458dc32008a2a3bdd0ef557d21b2e8970c
vquennessen <viquennessen@gmail.com>
2025-05-07 17:19:38 -0700

    added hatchling emergence success to thermal reaction norm figure (fig 3)

--- code/thermal_reaction_norm_examples.R
+++ code/thermal_reaction_norm_examples.R
@@ -4,6 +4,11 @@ setwd("~/Projects/iliketurtles3")
 # load libraries
 library(ggplot2)
 
+# emergence success variables
+hatch_success_A <- 0.86                   # logistic by temp - A
+hatch_success_k <- -1.7                   # logistic by temp - beta
+hatch_success_t0 <- 32.7                  # logistic by temp - t0
+
 # temperatures
 x <- seq(from = 20, to = 40, length = 100)
 
@@ -17,10 +22,16 @@ t_piv2 <- 29.2
 k2 <- -0.56
 y2 <- 1/(1 + exp(-k2*(x - t_piv2)))
 
+# emergence success
+ES <- hatch_success_A / (1 + exp(-hatch_success_k * (x - hatch_success_t0)))
+
+
 # make dataframe
 TRN <- data.frame(Temperature = x,
-                  Population = rep(c('West Africa', 'Suriname'), each = 100),
-                  Proportion_Male = c(y1, y2))
+                  Population = rep(c('West Africa', 
+                                     'Suriname', 
+                                     'Emergence Success'), each = 100),
+                  Proportion_Male = c(y1, y2, ES))
 
 # WA <- subset(TRN, Model == 'West Africa')
 # SN <- subset(TRN, Model == 'Suriname')
@@ -31,18 +42,18 @@ fig <- ggplot(data = TRN, aes(x = Temperature, y = Proportion_Male,
   geom_hline(yintercept = c(0.05, 0.50, 0.95), lwd = 1, lty = 1) +
   geom_vline(xintercept = 29.2, col = 'gray50', lwd = 1, lty = 1) +
   geom_vline(xintercept = 31.8, col = 'gray80', lwd = 1, lty = 1) +
-  geom_vline(xintercept = 27, col = '#00BFC4', lty = 5, 
+  geom_vline(xintercept = 27, col = '#6600FF', lty = 5, 
              lwd = 0.75, alpha = 0.5) +
-  geom_vline(xintercept = 31.3, col = '#00BFC4', lty = 5, 
+  geom_vline(xintercept = 31.3, col = '#6600FF', lty = 5, 
              lwd = 0.75, alpha = 0.5) +
-  geom_vline(xintercept = 34.4, col = '#F8766D', lty = 4, 
+  geom_vline(xintercept = 34.4, col = '#FF3300', lty = 4, 
              lwd = 0.75, alpha = 0.5) +
-  geom_vline(xintercept = 23.8, col = '#F8766D', lty = 4, 
+  geom_vline(xintercept = 23.8, col = '#FF3300', lty = 4, 
              lwd = 0.75, alpha = 0.5) +
   geom_line(lwd = 2) +
-  scale_color_manual(values = c('#F8766D', '#00BFC4')) +
-  scale_linetype_manual(values = c(4, 5)) +
-  ylab('Proportion hatchlings male') +
+  scale_color_manual(values = c('#33CC33', '#FF3300', '#6600FF')) +
+  scale_linetype_manual(values = c(1, 4, 5)) +
+  ylab('Proportion hatchlings male \n Hatchling emergence success') +
   xlab('Temperature (\u00B0C)') +
   theme_bw() +
   theme(axis.text = element_text(size = 12), 
@@ -54,11 +65,16 @@ fig <- ggplot(data = TRN, aes(x = Temperature, y = Proportion_Male,
         # # legend.position.inside = c(0.8, 0.75),
         # legend.key.width = unit(9.63, "line"), 
         legend.position = 'none') +
-  annotate("text", x = 39.4, y = 0.1, label = "0.05", size = 5) +
-  annotate("text", x = 39.4, y = 0.55, label = "0.50", size = 5) +
-  annotate("text", x = 39.4, y = 1.00, label = "0.95", size = 5) +
-  annotate("text", x = 28.1, y = 0.45, label = "29.2\u00B0C", size = 5) +
-  annotate("text", x = 33, y = 1.00, label = "31.8\u00B0C", size = 5)
+  annotate("label", x = 39.4, y = 0.1, label = "0.05", size = 5, 
+           label.size = 0) +
+  annotate("label", x = 39.4, y = 0.55, label = "0.50", size = 5, 
+           label.size = 0) +
+  annotate("label", x = 39.4, y = 1.00, label = "0.95", size = 5, 
+           label.size = 0) +
+  annotate("label", x = 28.4, y = 0, label = "29.2\u00B0C", size = 5, 
+           label.size = 0) +
+  annotate("label", x = 32.65, y = 1, label = "31.8\u00B0C", size = 5, 
+           label.size = 0)
 
 
 fig
