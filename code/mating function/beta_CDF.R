0b2377de670ad5e2a469a90684d85b5ebba19ac6
vquennessen <viquennessen@gmail.com>
2025-06-05 19:06:06 -0700

    updated legend title on figure 2 (hypothetical mating functions)

--- code/mating function/beta_CDF.R
+++ code/mating function/beta_CDF.R
@@ -48,7 +48,7 @@ fig <- ggplot(data = DF, aes(x = Operational_Sex_Ratio,
   scale_color_manual(values = rev(colors)[-1], 
                      labels = rev(OSRs)) +
   # for replacement legend - delete for legend with beta values
-  labs(color = 'OSR needed \n for 99% \n reproductive \n success') +
+  labs(color = 'Minimum \n OSR needed \n for 99% \n reproductive \n success') +
   ylab('Reproductive Success') +
   xlab('Operational Sex Ratio (Proportion Male)') +
   ggtitle('Hypothetical mating functions') +
