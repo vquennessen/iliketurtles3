4d0ccde348f2c88d0d4f0a5bd3ef3c7a36a99465
vquennessen <viquennessen@gmail.com>
2025-08-28 15:43:16 -0700

    some evolution runs P/GM evol piv normal/high H

--- code/cluster_runs.R
+++ code/cluster_runs.R
@@ -30,23 +30,26 @@ source('conservation.R')
 #             'GM_evol_piv', 'GM_evol_piv_high_H',
 #             'GM_evol_threshold', 'GM_evol_threshold_high_H')
 # models <- c('P_conservation', 'GM_conservation')
-# models <- c('P_base', 'GM_base')
-models <- c('P_evol_piv', 'GM_evol_piv', 
-            'P_evol_piv_high_H', 'GM_evol_piv_high_H')
+models <- c('P_evol_piv_high_H')
+# models <- c('P_evol_piv', 'GM_evol_piv', 
+#             'P_evol_piv_high_H', 'GM_evol_piv_high_H')
 
 # years to run the model for
 years <- 100
 
 # total temp increases
-scenarios <- years / 100 * c(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5)
-# scenarios <- c(0.5)
+# scenarios <- c(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5)
+scenarios <- c(2.5, 2, 3.5, 3.5, 3.5, 3.5, 3, 3, 3, 4.5, 4.5, 4.5, 4.5, 
+               4, 4, 4, 4, 4, 5, 5, 5)
 
 # OSR values to get full fertilization of females
-OSRs <- c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.49)
-# OSRs <- c(0.49, 0.35, 0.3, 0.25, 0.15)
+# OSRs <- c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.49)
+# OSRs <- c(0.35)
 
 # mating function beta values
-betas <- as.numeric(OSRs_to_betas(OSRs))
+# betas <- as.numeric(OSRs_to_betas(OSRs))
+betas <- c(3.82, 3.82, 3.82, 5.02, 6.64, 9.01, 3.82, 5.02, 6.64, 3.82, 5.02, 
+           6.64, 9.01, 3.82, 5.02, 6.64, 9.01, 12.91, 5.02, 20.63, 43.7)
 
 # how many clutches to do conservation action on
 # intensity <- c(0.1, 0.2, 0.3, 0.4, 0.5)
@@ -60,13 +63,22 @@ frequency <- c(1)
 num_sims <- c(10000)
 
 # make dataframe of all combinations of arguments
-DF <- expand.grid(models, 
-                  scenarios, 
-                  betas, 
-                  years, 
-                  num_sims, 
-                  intensity, 
-                  frequency) %>% 
+# DF <- expand.grid(models, 
+#                   scenarios, 
+#                   betas, 
+#                   years, 
+#                   num_sims, 
+#                   intensity, 
+#                   frequency) %>% 
+#   arrange(Var2, Var3)
+
+DF <- data.frame(Var1 = models, 
+                 Var2 = scenarios, 
+                 Var3 = betas, 
+                 Var4 = years, 
+                 Var5 = num_sims, 
+                 Var6 = intensity, 
+                 Var7 = frequency) %>%
   arrange(Var2, Var3)
 
 # initialize empty arguments list
@@ -82,4 +94,4 @@ for (i in 1:nrow(DF)) {
 ########### do the runs ########################################################
 mclapply(X = arguments, 
          FUN = run_base_model, 
-         mc.cores = 50)
+         mc.cores = 25)
