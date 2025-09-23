55ee06cb24c7f94598a21f75d8b5324ab1193d76
vquennessen <viquennessen@gmail.com>
2025-09-23 14:47:54 -0700

    new ideals heatmaps to show with and without emergence success and difference

--- code/plot_ideals_heatmap.R
+++ code/plot_ideals_heatmap.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(tidyverse)
 library(readr)
 library(ggpattern)
+library(patchwork)
 
 ##### calculate ideal temperatures (adjusted) ##################################
 
@@ -13,7 +14,7 @@ source('~/Projects/iliketurtles3/code/mating function/OSRs_to_betas.R')
 
 # operational proportion male
 OSRs <- c(0.49, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05)
-xFs <- round(1/OSRs - 1, 2)
+# xFs <- round(1/OSRs - 1, 2)
 betas <- as.numeric(OSRs_to_betas(OSRs))
 
 # hatchling sex ratios * emergence success
@@ -45,7 +46,7 @@ hatchlings <- data.frame(TRT = rep(TRTs, each = length(betas) * length(temps)),
                                          times = length(TRTs) * length(betas)), 
                          PSR = c(rep(narrow_TRT_pM, times = length(betas)), 
                                  rep(wide_TRT_pM, times = length(betas)))) %>%
-  mutate(xF = round(1/OSR - 1, 2)) %>%
+  # mutate(xF = round(1/OSR - 1, 2)) %>%
   mutate(nM = 100 * PSR / M_remigration) %>%
   mutate(nF = 100 * (1 - PSR) / F_remigration) %>%
   mutate(breeding_success = pbeta(q = 2*(nM / (nM + nF)), 
@@ -56,17 +57,29 @@ hatchlings <- data.frame(TRT = rep(TRTs, each = length(betas) * length(temps)),
 
 ideals <- hatchlings %>%
   group_by(TRT, Beta) %>%
-  filter(nHatchlings == max(nHatchlings)) %>%
-  mutate(xF = round(1/OSR - 1, 2)) 
+  filter(nHatchlings == max(nHatchlings))
+  # mutate(xF = round(1/OSR - 1, 2)) 
 
 # make values factors
 ideals$OSR <- factor(ideals$OSR, levels = OSRs)
 ideals$TRT <- factor(ideals$TRT, levels = TRTs)
-ideals$xF <- factor(ideals$xF, levels = unique(ideals$xF))
+# ideals$xF <- factor(ideals$xF, levels = unique(ideals$xF))
 
 # save as table
-save(ideals, file = '~/Projects/iliketurtles3/output/ideals.Rdata')
+save(ideals, 
+     file = '~/Projects/iliketurtles3/output/ideals.Rdata')
 
+ideals_without_emergence <- hatchlings %>%
+  group_by(TRT, Beta) %>%
+  filter(nEggs == max(nEggs))
+
+# make values factors
+ideals_without_emergence$OSR <- factor(ideals_without_emergence$OSR, levels = OSRs)
+ideals_without_emergence$TRT <- factor(ideals_without_emergence$TRT, levels = TRTs)
+
+# save as table
+save(ideals_without_emergence, 
+     file = '~/Projects/iliketurtles3/output/ideals_without_emergence.Rdata')
 
 ##### make the figure ##########################################################
 
@@ -86,22 +99,19 @@ load("~/Projects/iliketurtles3/output/ideals.Rdata")
 
 # adjust dataframe to get other useful columns
 to_plot <- ideals %>%
-  mutate(Above_init_temp = as.character(Temp > 31.8)) %>%
-  mutate(temps_below = replace(Temp, Temp <= 31.8, '')) %>%
-  mutate(temps_above = replace(Temp, Temp > 31.8, ''))
+  # mutate(Above_init_temp = as.character(Temp > 31.8)) %>%
+  # mutate(temps_below = replace(Temp, Temp <= 31.8, '')) %>%
+  # mutate(temps_above = replace(Temp, Temp > 31.8, '')) %>%
+  mutate(labs = paste(Temp, '\n (', round(PSR, 2), ')', sep = ''))
 
 # actually do the heatmap thing
 ideal_temps_heatmap <- ggplot(data = to_plot, 
                               aes(x = OSR, 
                                   y = TRT, 
-                                  fill = Temp, 
-                                  pattern = Above_init_temp)) +
+                                  fill = Temp)) +
   geom_tile(color = 'white') +
-  # scale_x_discrete(labels = xlabs) +
-  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', 
-                       midpoint = 29.2) +
   labs(fill = "Incubation \n temperature \n (\u00B0C)") +
-  xlab("Minimum OSR required for 99% female reproductive success") +
+  xlab("Minimum OSR required for 99% female reproductive success \n (associated hatchling sex ratio as proportion male)") +
   scale_y_discrete(labels = c("Narrow \n transitional \n range", 
                               "Wide \n transitional \n range"
   )) +
@@ -114,126 +124,153 @@ ideal_temps_heatmap <- ggplot(data = to_plot,
   theme(axis.text = element_text(size = 9)) +
   theme(strip.text = element_text(size = 9)) +
   theme(legend.title = element_text(size = 9)) +
-  geom_text(aes(label = round(as.numeric(temps_below), 2)), 
-            size = 3) +
-  geom_text(aes(label = round(as.numeric(temps_above), 2)), 
+  geom_text(aes(label = labs), 
             size = 3, 
             fontface = 'bold')
+  # geom_text(aes(label = round(as.numeric(temps_above), 2)), 
+  #           size = 3)
 
 # guides(pattern = 'none')
 
 ideal_temps_heatmap
 
-ggsave(ideal_temps_heatmap,
-       file = '~/Projects/iliketurtles3/figures/ideal_temps_heatmap.png',
-       width = 8, height = 3)
+# ggsave(ideal_temps_heatmap,
+#        file = '~/Projects/iliketurtles3/figures/ideal_temps_heatmap.png',
+#        width = 8, height = 3)
 
 ##### ideal temps (not adjusted by emergence success) ##########################
-# 
-# # osrs
-# osrs <- c(0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.49)
-# betas <- as.numeric(OSRs_to_betas(osrs))
-# # betas <- c(43.71, 20.64, 12.92, 9.02, 6.65, 5.03, 3.83, 2.87, 2.01, 1)
-# 
-# # ideal hatchling sex ratios based on mating system
-# IHSR <- (1.47 * osrs) / (1.47 * osrs + 3.87 * (1 - osrs))
-# # [1] 0.01960000 [2] 0.04049587 [3] 0.06282051 [4] 0.08672566 [5] 0.11238532 
-# # [6] 0.14000000 [7] 0.16980198 [8] 0.20206186 [9] 0.23709677 [10] 0.26737194
-# 
-# # temperatures that give those IHSRs based on thermal reaction norm
-# # narrow TRT population; k = -1.34, pivotal temp = 29.2
-# ITemps_narrow <- (log( 1 / IHSR - 1) + 1.34 * 29.2) / 1.34
-# # [1] 32.11972 [2] 31.56210 [3] 31.21686 [4] 30.95693 [5] 30.74224 
-# # [6] 30.55469 [7] 30.38435 [8] 30.22497 [9] 30.07214 [10] 29.95224
-# 
-# # wide TRT population, k = -0.561, pivotal temp = 29.2
-# ITemps_wide <- (log( 1 / IHSR - 1) + 0.561 * 29.2) / 0.561
-# # [1] 36.17403 [2] 34.84210 [3] 34.01746 [4] 33.39659 [5] 32.88379 
-# # [6] 32.43581 [7] 32.02893 [8] 31.64823 [9] 31.28318 [10] 30.99679
-# 
-# 
-# # ideal values
-# ideals <- data.frame(
-#   Min_OSR = as.numeric(osrs), 
-#   Ideal_Hatchling_Sex_Ratio = round(as.numeric(IHSR), 3), 
-#   Narrow_TRT = round(as.numeric(ITemps_narrow), 3),
-#   Wide_TRT = round(as.numeric(ITemps_wide), 3)
-# ) 
-# 
-# # save as table
-# write_csv(ideals, file = '../output/ideals.csv')
-# save(ideals, file = '../output/ideals.Rdata')
-# 
-# # load ideals object
-# # ideals <- read.csv('output/ideals.csv')
-# load("~/Projects/iliketurtles3/output/ideals.Rdata")
-# 
-# # xaxis labels
-# xlabs <- paste(unique(ideals$Min_OSR), '\n (', 
-#                unique(ideals$Ideal_Hatchling_Sex_Ratio), ')', sep = '')
-# 
-# # apply to factor
-# ideals$xlabs <- as.factor(xlabs)
-# 
-# # adjust dataframe to get other useful columns
-# to_plot <- ideals %>%
-#   pivot_longer(cols = c(3, 4), 
-#                names_to = "Population", 
-#                values_to = "Temperature") %>%
-#   mutate(Above_init_temp = as.character(Temperature > 31.8)) %>%
-#   mutate(temps_above = replace(Temperature, Temperature <= 31.8, '')) %>%
-#   mutate(temps_below = replace(Temperature, Temperature > 31.8, ''))
-# 
-# # actually do the heatmap thing
-# ideal_temps_heatmap <- ggplot(data = to_plot, 
-#          aes(x = as.factor(Min_OSR), 
-#            y = Population, 
-#            fill = Temperature, 
-#            pattern = Above_init_temp)) +
-#   geom_tile(color = 'white') +
-#   scale_x_discrete(labels = xlabs) +
-#   scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', 
-#                       midpoint = 29.2) +
-#   labs(fill = "Incubation \n temperature \n (\u00B0C)") +
-#   xlab("Minimum OSR required for 99% female reproductive success \n (Associated proportion of hatchlings produced that are male)") +
-#   scale_y_discrete(labels = c("Narrow TRT \n population ", 
-#                               "Wide TRT \n population "
-#                               )) +
-#   theme_bw() +
-#   theme(panel.grid.major = element_blank(), 
-#         panel.grid.minor = element_blank()) +
-#   theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
-#   theme(axis.title.x = element_text(size = 11, vjust = -3)) +
-#   theme(axis.title.y = element_blank()) +
-#   theme(axis.text = element_text(size = 9)) +
-#   theme(strip.text = element_text(size = 9)) +
-#   theme(legend.title = element_text(size = 9)) +
-#   # annotate("rect", 
-#   #          xmin = c(1.5, 1.5, 1.5, 7.5), 
-#   #          xmax = c(10.5, 7.5, 1.5, 7.5), 
-#   #          ymin = c(0.5, 1.5, 0.5, 1.5), 
-#   #          ymax = c(0.5, 1.5, 1.5, 2.5), 
-#   #          colour = "black", linewidth = 1) +
-#     # geom_tile_pattern(pattern_color = NA,
-#     #                   pattern_fill = "black",
-#     #                   pattern_alpha = 0.5,
-#     #                   pattern_angle = 45,
-#     #                   pattern_density = 0.125,
-#     #                   pattern_spacing = 0.05, 
-#     #                   pattern_key_scale_factor = 1) +
-#     # scale_pattern_manual(values = c('FALSE' = "stripe", 'TRUE' = "none")) +
-#   geom_text(aes(label = round(as.numeric(temps_above), 2)), 
-#             size = 3) +
-#   geom_text(aes(label = round(as.numeric(temps_below), 2)), 
-#             size = 3, 
-#             fontface = 'bold')
-# 
-#   # guides(pattern = 'none')
-# 
-# ideal_temps_heatmap
-# 
-# 
-# 
-# ggsave(ideal_temps_heatmap, 
-#        file = '~/Projects/iliketurtles3/figures/ideal_temps_heatmap.png', 
+
+# load in object
+load("~/Projects/iliketurtles3/output/ideals_without_emergence.Rdata")
+
+# adjust dataframe to get other useful columns
+to_plot2 <- ideals_without_emergence %>%
+  # mutate(Above_init_temp = as.character(Temp > 31.8)) %>%
+  # mutate(temps_below = replace(Temp, Temp <= 31.8, '')) %>%
+  # mutate(temps_above = replace(Temp, Temp > 31.8, '')) %>%
+  mutate(labs = paste(Temp, '\n (', round(PSR, 2), ')', sep = ''))
+
+# actually do the heatmap thing
+ideal_temps_without_emergence_heatmap <- ggplot(data = to_plot2, 
+                              aes(x = OSR, 
+                                  y = TRT, 
+                                  fill = Temp)) +
+  geom_tile(color = 'white') +
+  # scale_x_discrete(labels = xlabs) +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', 
+                       midpoint = 29.2) +
+  labs(fill = "Incubation \n temperature \n (\u00B0C)") +
+  xlab("Minimum OSR required for 99% female reproductive success \n (associated hatchling sex ratio as proportion male)") +
+  scale_y_discrete(labels = c("Narrow \n transitional \n range", 
+                              "Wide \n transitional \n range"
+  )) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
+  theme(axis.title.x = element_text(size = 11, vjust = -3)) +
+  theme(axis.title.y = element_blank()) +
+  theme(axis.text = element_text(size = 9)) +
+  theme(strip.text = element_text(size = 9)) +
+  theme(legend.title = element_text(size = 9)) +
+  geom_text(aes(label = labs), 
+            size = 3, 
+            fontface = 'bold')
+
+# guides(pattern = 'none')
+
+ideal_temps_without_emergence_heatmap
+
+# ggsave(ideal_temps_without_emergence_heatmap,
+#        file = '~/Projects/iliketurtles3/figures/ideal_temps_without_emergence_heatmap.png',
 #        width = 8, height = 3)
+
+##### joint figure #############################################################
+
+B <- ideal_temps_heatmap +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', 
+                       midpoint = 29.2, limits = c(29, 35)) + 
+  guides(fill = 'none')  + 
+  labs(tag = '(B)') +
+  theme(plot.tag.position = c(0, 1), 
+        plot.tag = element_text(hjust = 0, vjust = 1, size = 12, face = 'bold')) 
+
+B
+
+A <- ideal_temps_without_emergence_heatmap +
+  theme() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red', 
+                       midpoint = 29.2, limits = c(29, 35)) +
+  labs(tag = '(A)') +
+  theme(axis.title.x = element_blank(), 
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank(),
+        plot.tag.position = c(0, 1), 
+        plot.tag = element_text(hjust = 0, vjust = 1, size = 12, face = 'bold'))
+
+joint_fig <- A / plot_spacer() / B +
+  plot_layout(ncol = 1, heights = c(1, -0.25, 1), guides = 'collect')
+
+joint_fig
+
+ggsave(joint_fig,
+       file = '~/Projects/iliketurtles3/figures/ideal_temps_joined_heatmap.png',
+       width = 7.5, height = 5)
+
+##### joint fig + difference between the two ###################################
+
+joint_to_plot <- to_plot %>%
+  ungroup() %>%
+  mutate(WE_Temp = to_plot2$Temp) %>%
+  mutate(WE_PSR = to_plot2$PSR) %>%
+  mutate(diff_Temp = round(WE_Temp - Temp, 2)) %>%
+  mutate(diff_PSR = round(WE_PSR - PSR, 2)) %>%
+  mutate(diff_labs = paste(diff_Temp, '\n (', diff_PSR, ')', sep = ''))
+
+
+joint_fig_diff <- ggplot(data = joint_to_plot, 
+         aes(x = OSR, 
+             y = TRT, 
+             fill = diff_Temp)) +
+  geom_tile(color = 'white') +
+  scale_fill_gradient2(high = 'dodgerblue3', mid = 'white', low = 'red', 
+                       midpoint = 0) +
+  labs(fill = "Difference in \n incubation \n temperature \n (\u00B0C)") +
+  xlab("Minimum OSR required for 99% female reproductive success \n (associated hatchling sex ratio as proportion male)") +
+  scale_y_discrete(labels = c("Narrow \n transitional \n range", 
+                              "Wide \n transitional \n range"
+  )) +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  theme(plot.margin = unit(c(0.5, 0.25, 1, 1), units = 'cm')) +
+  theme(axis.title.x = element_text(size = 11, vjust = -3)) +
+  theme(axis.title.y = element_blank()) +
+  theme(axis.text = element_text(size = 9)) +
+  theme(strip.text = element_text(size = 9)) +
+  theme(legend.title = element_text(size = 9)) +
+  geom_text(aes(label = diff_Temp), 
+            size = 3, 
+            fontface = 'bold')
+
+joint_fig_diff
+
+B2 <- B +
+  theme(axis.title.x = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank())
+
+joint_fig2 <- A / plot_spacer() / B2 +
+  plot_layout(ncol = 1, heights = c(1, -0.1, 1), guides = 'collect')
+
+final_fig <- joint_fig2 / plot_spacer() / joint_fig_diff +
+  plot_layout(ncol = 1, heights = c(1, -0.45, 1, -0.45, 1)) +
+  labs(tag = '(C)')  +
+  theme(plot.tag.position = c(0, 1), 
+        plot.tag = element_text(hjust = 0, vjust = 1, size = 12, face = 'bold'))
+
+final_fig
+
+ggsave(final_fig,
+       file = '~/Projects/iliketurtles3/figures/ideal_temps_all_heatmap.png',
+       width = 8, height = 6)
