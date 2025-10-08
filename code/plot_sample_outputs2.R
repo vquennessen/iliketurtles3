a55aab2ee418cda3fd3df7e03188e6b8b4b1010e
vquennessen <viquennessen@gmail.com>
2025-10-07 19:04:08 -0700

    adjusting sample outputs figure

--- code/plot_sample_outputs2.R
+++ code/plot_sample_outputs2.R
@@ -14,6 +14,7 @@ library(ggh4x)
 # source functions and load data
 source('~/Projects/iliketurtles3/code/mating function/OSRs_to_betas.R')
 load("~/Projects/iliketurtles3/output/joined_outputs.Rdata")
+load("~/Projects/iliketurtles3/output/ideals_adjusted.Rdata")
 
 ##### clean up data ############################################################
 
@@ -24,78 +25,90 @@ scenarios <- c('0.5\u00B0C', '4.5\u00B0C')
 osrs <- c(0.1, 0.35)
 betas <- OSRs_to_betas(osrs)
 
+
+# add ideals to dataframe
+clean_ideals <- ideals %>%
+  mutate(Beta = as.character(Beta)) %>%
+  rename('Ideal_Temp' = 'Temp') %>%
+  rename('Ideal_PSR' = 'PSR')
+
 examples_to_plot <- joined_outputs %>%
+  mutate(Beta = as.character(Beta)) %>%
+  full_join(clean_ideals) %>%
   filter(Scenario %in% scenarios) %>%
-  filter(OSR %in% factor(osrs))
+  filter(OSR %in% factor(osrs)) %>%
+  mutate(Scenario = factor(Scenario, levels = scenarios)) %>%
+  mutate(OSR = factor(OSR, levels = osrs)) %>%
+  mutate(TRT = factor(TRT))
 
-  # make scenario and OSR a factor
-examples_to_plot$Scenario <- factor(examples_to_plot$Scenario, 
-                                   levels = as.factor(unique(examples_to_plot$Scenario)))
-examples_to_plot$OSR <- factor(examples_to_plot$OSR, 
-                              levels = as.factor(unique(examples_to_plot$OSR)))
-examples_to_plot$TRT <- factor(examples_to_plot$TRT, 
-                              levels = as.factor(unique(examples_to_plot$TRT)))  
 
-# ideal hatchling proportions male
-ideal_0.1 <- 0.09997
-ideal_0.35 <- 0.24497
+#   # make scenario and OSR a factor
+# examples_to_plot$Scenario <- factor(examples_to_plot$Scenario, 
+#                                    levels = as.factor(unique(examples_to_plot$Scenario)))
+# examples_to_plot$OSR <- factor(examples_to_plot$OSR, 
+#                               levels = as.factor(unique(examples_to_plot$OSR)))
+# examples_to_plot$TRT <- factor(examples_to_plot$TRT, 
+#                               levels = as.factor(unique(examples_to_plot$TRT)))  
 
-ideal_0.1_xF <- round(1/ideal_0.1 - 1, 2)
-ideal_0.35_xF <- round(1/ideal_0.35 - 1, 2)
-
-ideal_0.1_xM <- round(ideal_0.1/(1 - ideal_0.1), 2)
-ideal_0.35_xM <- round(ideal_0.35/(1 - ideal_0.35), 2)
+# # ideal hatchling proportions male
+# ideal_0.1 <- 0.09997
+# ideal_0.35 <- 0.24497
+# 
+# ideal_0.1_xF <- round(1/ideal_0.1 - 1, 2)
+# ideal_0.35_xF <- round(1/ideal_0.35 - 1, 2)
+# 
+# ideal_0.1_xM <- round(ideal_0.1/(1 - ideal_0.1), 2)
+# ideal_0.35_xM <- round(ideal_0.35/(1 - ideal_0.35), 2)
 
 ##### plot 1: hatchling sex ratios #############################################
 
 HSR <- ggplot(data = examples_to_plot, 
-            aes(x = Year, 
-                y = Hatchling_Sex_Ratio_Median,
-                # y = Hatchling_xF_Median, 
-                col = Scenario, 
-                lty = Mating_Function)) +
+              aes(x = Year, 
+                  y = Hatchling_Sex_Ratio_Median)) +
   geom_ribbon(aes(ymin = Hatchling_Sex_Ratio_Q25,
                   ymax = Hatchling_Sex_Ratio_Q75,
-  # geom_ribbon(aes(ymin = Hatchling_xF_Q25,
-  #                 ymax = Hatchling_xF_Q75,
                   col = Scenario,
-                  fill = Scenario, 
-                  lty = Mating_Function),
-              alpha = 0.25,
+                  fill = Scenario),
+              alpha = 0.25, 
               show.legend = FALSE) +
-  scale_color_manual(values = c('#00BFC4', '#F8766D')) +
+  scale_color_manual(values = c('#00BFC4', '#F8766D'), 
+                     name = 'Emergence \n success') +
   scale_fill_manual(values = c('#00BFC4', '#F8766D')) +
-  
+  geom_line(aes(x = Year, 
+                y = Ideal_PSR, 
+                lty = Mating_Function), 
+            col = 'black', 
+            show.legend = FALSE) +
+  geom_path(aes(col = Scenario, 
+                lty = Mating_Function), 
+            linewidth = 0.75, 
+            show.legend = FALSE) +
+  scale_linetype_manual(values = c(1, 2)) +
+  geom_point(data = examples_to_plot %>% filter(Year %% 3 == 0), 
+             aes(x = Year, 
+                 y = Emergence_Success, 
+                 col = Scenario, 
+                 fill = Scenario), 
+             shape = 3,
+             size = 1) +
   facet_grid(cols = vars(TRT)) +
-  geom_hline(yintercept = ideal_0.1, lty = 2) +
-  geom_hline(yintercept = ideal_0.35, lty = 1) +
-  # geom_hline(yintercept = ideal_0.1_xF, lty = 2) +
-  # geom_hline(yintercept = ideal_0.35_xF, lty = 1) +
-  geom_path(linewidth = 0.75) +
-  geom_line(aes(x = Year, y = Emergence_Success, col = Scenario), 
-            lty = 3, linewidth = 1) +
-  # ylim(0, 100) +
-  guides(col = 'none', fill = 'none', lty = 'none') +
   ylab("(A) Median \n hatchling sex ratio") +
-  # ylab("(A) Median hatchling \n sex ratio (xF:1M)") +
-  labs(x = NULL) +
+  # guides(fill = guide_legend(override.aes = list(color = NA)), 
+  #        lty = 'none') +
   theme_bw() +
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(), 
         axis.ticks.x = element_blank())
-# 
-#   geom_text(data = Alabs, 
-#             aes(x = Year, y = Hatchling_Sex_Ratio_Median, label = Labs))
 
 HSR
 
 ##### plot 2: hatchling abundance ##############################################
 
 HA <- ggplot(data = examples_to_plot, 
-            aes(x = Year, 
-                y = Hatchling_Abundance_Median, 
-                col = Scenario, 
-                lty = Mating_Function)) +
+             aes(x = Year, 
+                 y = Hatchling_Abundance_Median, 
+                 col = Scenario, 
+                 lty = Mating_Function)) +
   geom_ribbon(aes(ymin = Hatchling_Abundance_Q25,
                   ymax = Hatchling_Abundance_Q75,
                   col = NULL,
@@ -124,15 +137,15 @@ HA
 ##### plot 2: operational sex ratios ###########################################
 
 OSR <- ggplot(data = examples_to_plot, 
-            aes(x = Year, 
-                y = Mature_Sex_Ratio_Median,
-                # y = Mature_xF_Median, 
-                col = Scenario, 
-                lty = Mating_Function)) +
+              aes(x = Year, 
+                  y = Mature_Sex_Ratio_Median,
+                  # y = Mature_xF_Median, 
+                  col = Scenario, 
+                  lty = Mating_Function)) +
   geom_ribbon(aes(ymin = Mature_Sex_Ratio_Q25,
                   ymax = Mature_Sex_Ratio_Q75,
-  # geom_ribbon(aes(ymin = Mature_xF_Q25,
-  #                 ymax = Mature_xF_Q75,
+                  # geom_ribbon(aes(ymin = Mature_xF_Q25,
+                  #                 ymax = Mature_xF_Q75,
                   col = NULL,
                   fill = Scenario),
               alpha = 0.25,
@@ -159,10 +172,10 @@ OSR
 ##### plot 4: breeding success #################################################
 
 BS <- ggplot(data = examples_to_plot, 
-            aes(x = Year, 
-                y = Breeding_Success_Median, 
-                col = Scenario, 
-                lty = Mating_Function)) +
+             aes(x = Year, 
+                 y = Breeding_Success_Median, 
+                 col = Scenario, 
+                 lty = Mating_Function)) +
   geom_ribbon(aes(ymin = Breeding_Success_Q25,
                   ymax = Breeding_Success_Q75,
                   col = NULL,
@@ -189,10 +202,10 @@ BS
 ##### plot 5: mature abundance #################################################
 
 MA <- ggplot(data = examples_to_plot, 
-            aes(x = Year, 
-                y = Mature_Abundance_Median, 
-                col = Scenario, 
-                lty = Mating_Function)) +
+             aes(x = Year, 
+                 y = Mature_Abundance_Median, 
+                 col = Scenario, 
+                 lty = Mating_Function)) +
   geom_ribbon(aes(ymin = Mature_Abundance_Q25,
                   ymax = Mature_Abundance_Q75,
                   col = NULL,
@@ -217,10 +230,10 @@ MA
 ##### plot 6: median lambda ####################################################
 
 lambda <- ggplot(data = examples_to_plot, 
-                aes(x = Year, 
-                    y = Lambda_10yr_median, 
-                    color = Scenario, 
-                    linetype = Mating_Function)) + 
+                 aes(x = Year, 
+                     y = Lambda_10yr_median, 
+                     color = Scenario, 
+                     linetype = Mating_Function)) + 
   # facet_grid(cols = vars(TRT), rows = vars(facet_labels)) +
   facet_grid(cols = vars(TRT)) +
   geom_hline(yintercept = 1) +
@@ -238,7 +251,7 @@ lambda <- ggplot(data = examples_to_plot,
   guides(col = 'none', fill = 'none', lty = 'none') +
   theme_bw() +
   theme(strip.text = element_blank())
-  
+
 lambda
 
 ##### final figure #############################################################
