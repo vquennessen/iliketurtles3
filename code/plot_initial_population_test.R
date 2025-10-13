2f9c1811fe93b734f8516938501ae98d5ab6a618
vquennessen <viquennessen@gmail.com>
2025-10-12 21:01:05 -0700

    new script for plotting SADs by sex

--- code/plot_initial_population_test.R
+++ code/plot_initial_population_test.R
@@ -8,114 +8,150 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 
-##### nsims 1000 burn-in 100 ###################################################
+# plotting parameters
+Nsims <- c(100, 1000, 10000)
+Burn_ins <- c(100, 200, 300)
 
-N1000B100 <- raw_N1000B100 %>%
-  group_by(Model, Beta, Sex, Age) %>%
-  summarise(prop_median = median(Proportion), 
-         prop_Q25 = quantile(Proportion, probs = 0.25), 
-         prop_Q75 = quantile(Proportion, probs = 0.75), 
-         abundance_median = median(Abundance), 
-         abundance_Q25 = quantile(Abundance, probs = 0.25), 
-         abundance_Q75 = quantile(Abundance, probs = 0.75), 
-         .groups = 'drop')
+SAD <- data.frame(Model = NULL, 
+                  Beta = NULL, 
+                  Sex = NULL, 
+                  Age = NULL, 
+                  Abundance = NULL,
+                  Proportion = NULL,
+                  Adjusted_Abundance = NULL, 
+                  prop_median = NULL,
+                  prop_Q25 = NULL,
+                  prop_Q75 = NULL,
+                  abundance_median = NULL,
+                  abundance_Q25 = NULL,
+                  abundance_Q75 = NULL)
 
-### proportions
-N1000B100_props <- ggplot(data = N1000B100, 
-                          aes(x = Age, 
-                              y = prop_median, 
-                              col = Model, 
-                              lty = factor(Beta))) +
-  facet_wrap(facets = vars(Sex), 
-             scale = 'free') +
-  geom_line() +
-  # ylim(c(0, 1)) +
-  geom_ribbon(aes(ymin = prop_Q25,
-                  ymax = prop_Q75,
-                  col = NULL,
-                  fill = factor(Model)),
-              alpha = 0.25,
-              show.legend = FALSE)
+for (n in 1:length(Nsims)) {
+  
+  nsims <- Nsims[n]
+  
+  for (b in 1:length(Burn_ins)) {
+    
+    burn_in <- Burn_ins[b]
+    
+    # # make facet panels have consistent y axes by sex across runs
+    # facet_bounds <- read.table(header = TRUE,
+    #                            text =  
+    #                              "Sex   ymin  ymax       breaks
+    #                           IF    0     0.4        3
+    #                           IM    0     0.05       3
+    #                           MF    0     0.00175    2
+    #                           MM    0     0.000125   2",
+    #                            stringsAsFactors = FALSE)
+    # 
+    # xylims <- with(facet_bounds,
+    #                data.frame(prop_median = c(ymin, ymax),
+    #                           Sex = c(Sex, Sex), 
+    #                           Model = 'P_base', 
+    #                           Beta = 1.17))
+    
+    # load in object
+    
+    if (file.exists(paste('~/Projects/iliketurtles3/output/SAD_n', nsims, 
+                          '_b', burn_in, '.Rdata', sep = ''))) {
+      
+      load(paste('~/Projects/iliketurtles3/output/SAD_n', nsims, 
+                 '_b', burn_in, '.Rdata', sep = ''))
+      
+      # build dataframe with median, IQR, etc.
+      DF <- SADdf %>%
+        mutate(Nsims = nsims) %>%
+        mutate(Burn_in = burn_in) %>%
+        filter(!is.na(Proportion)) %>%
+        filter(!is.na(Abundance)) %>%
+        group_by(Beta, Sex, Age, Model, Nsims, Burn_in) %>%
+        summarise(prop_median = median(Proportion), 
+                  prop_Q25 = quantile(Proportion, probs = 0.25), 
+                  prop_Q75 = quantile(Proportion, probs = 0.75), 
+                  abundance_median = median(Abundance), 
+                  abundance_Q25 = quantile(Abundance, probs = 0.25), 
+                  abundance_Q75 = quantile(Abundance, probs = 0.75), 
+                  .groups = 'drop') %>%
+        mutate(Nsims = paste('nsims = ', Nsims, sep = '')) %>%
+        mutate(Burn_in = paste('burn-in = ', Burn_in, sep = ''))
+      
+      SAD <- rbind(SAD, DF)
+      
+    }
+    
+  }
+  
+}
 
-N1000B100_props
+sexes <- c('IF', 'IM', 'MF', 'MM')
 
-### abundances
-N1000B100_abundance <- ggplot(data = N1000B100, 
-                              aes(x = Age, 
-                                  y = abundance_median, 
-                                  col = Model, 
-                                  lty = factor(Beta))) +
-  facet_wrap(facets = vars(Sex), 
-             scale = 'free') +
-  geom_line() +
-  geom_ribbon(aes(ymin = abundance_Q25,
-                  ymax = abundance_Q75,
-                  col = NULL,
-                  fill = factor(Model)),
-              alpha = 0.1,
-              show.legend = FALSE)
-
-N1000B100_abundance
-
-##### general SAD figs #########################################################
-
-nsims <- 1000
-burn_in <- 200
-
-load(paste('~/Projects/iliketurtles3/output/SAD_n', 
-           nsims, '_b', burn_in, '.Rdata', sep = ''))
-
-DF <- SAD %>%
-  filter(!is.na(Proportion)) %>%
-  filter(!is.na(Abundance)) %>%
-  # group_by(Model, Beta, Sex, Age) %>%
-  group_by(Beta, Sex, Age, Burn_in) %>%
-  summarise(prop_median = median(Proportion), 
-            prop_Q25 = quantile(Proportion, probs = 0.25), 
-            prop_Q75 = quantile(Proportion, probs = 0.75), 
-            abundance_median = median(Abundance), 
-            abundance_Q25 = quantile(Abundance, probs = 0.25), 
-            abundance_Q75 = quantile(Abundance, probs = 0.75), 
-            .groups = 'drop')
-
-### proportions
-Props <- ggplot(data = P_all_summarized, 
-                          aes(x = Age, 
-                              y = prop_median, 
-                              # col = Model, 
-                              # lty = factor(Beta)
-                              col = factor(Beta), 
-                              lty = factor(Burn_in)
-                              )) +
-  facet_wrap(facets = vars(Sex), 
-             scale = 'free') +
-  geom_line() +
-  # ylim(c(0, 1)) +
-  geom_ribbon(aes(ymin = prop_Q25,
-                  ymax = prop_Q75,
-                  col = NULL,
-                  # fill = factor(Model)),
-                  fill = factor(Beta)),
-              
-              alpha = 0.25,
-              show.legend = FALSE)
-
-Props
-
-### abundances
-N1000B100_abundance <- ggplot(data = N1000B100, 
-                              aes(x = Age, 
-                                  y = abundance_median, 
-                                  col = Model, 
-                                  lty = factor(Beta))) +
-  facet_wrap(facets = vars(Sex), 
-             scale = 'free') +
-  geom_line() +
-  geom_ribbon(aes(ymin = abundance_Q25,
-                  ymax = abundance_Q75,
-                  col = NULL,
-                  fill = factor(Model)),
-              alpha = 0.1,
-              show.legend = FALSE)
-
-N1000B100_abundance
+for (s in 1:length(sexes)) {
+  
+  sex <- sexes[s]
+  
+  to_plot <- SAD %>%
+    filter(Sex == sex)
+  
+  ### proportions
+  Props <- ggplot(data = to_plot, 
+                  aes(x = Age, 
+                      y = prop_median, 
+                      lty = Model,
+                      col = factor(Beta)
+                  )) +
+    facet_grid(rows = vars(Nsims), 
+               cols = vars(factor(Burn_in))) +
+    geom_line() +
+    geom_ribbon(aes(ymin = prop_Q25,
+                    ymax = prop_Q75,
+                    col = NULL,
+                    fill = factor(Beta)),
+                alpha = 0.15,
+                show.legend = FALSE) +
+    # geom_point(data = xylims, x = NA) +
+    ggtitle(paste('sex = ', sex, sep = ''))
+  
+  Props
+  
+  # ggsave(filename = paste('props_SAD_n', nsims, '_b', burn_in, '.png', sep = ''),
+  #        plot = Props, 
+  #        path = '~/Projects/iliketurtles3/figures/SAD', 
+  #        width = 10, height = 7)
+  
+  ggsave(filename = paste('SAD_props_', sex, '.png', sep = ''),
+         plot = Props,
+         path = '~/Projects/iliketurtles3/figures/SAD',
+         width = 10, height = 7)
+  
+  ### abundances
+  Abundances <- ggplot(data = to_plot, 
+                       aes(x = Age, 
+                           y = abundance_median, 
+                           lty = Model, 
+                           col = factor(Beta))) +
+    facet_grid(rows = vars(Nsims), 
+               cols = vars(factor(Burn_in))) +
+    geom_line() +
+    geom_ribbon(aes(ymin = abundance_Q25,
+                    ymax = abundance_Q75,
+                    col = NULL,
+                    fill = factor(Beta)),
+                alpha = 0.15,
+                show.legend = FALSE) +
+    ggtitle(paste('sex = ', sex, sep = ''))
+  
+  
+  Abundances
+  
+  # ggsave(filename = paste('abundances_SAD_n', nsims, '_b', burn_in, '.png', 
+  #                         sep = ''),
+  #        plot = Abundances, 
+  #        path = '~/Projects/iliketurtles3/figures/SAD', 
+  #        width = 10, height = 7)
+  
+  ggsave(filename = paste('SAD_abundances_', sex, '.png', sep = ''),
+         plot = Abundances,
+         path = '~/Projects/iliketurtles3/figures/SAD',
+         width = 10, height = 7)
+  
+}
